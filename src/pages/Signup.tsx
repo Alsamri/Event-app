90f3e913aeced49845315862c562c04542c4b190
Alsamri <azozid@yahoo.com>
2025-10-18 21:54:31 +0100

    cors webs

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/clerk-react";
 export default function Signup() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <SignUp path="/signup" signInUrl="/login" forceRedirectUrl="/" />
+      <SignUp path="/signup" routing="path" signInUrl="/login" forceRedirectUrl="/" />
     </div>
   );
 }
